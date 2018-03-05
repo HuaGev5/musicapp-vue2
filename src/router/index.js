3b65613c1f3bffb9e75660f7a870506de1fd3f48
HuaGev5 <550237190@qq.com>
2018-03-05 21:37:47 +0800

    huagecommit

--- src/router/index.js
+++ src/router/index.js
@@ -8,36 +8,43 @@ const Recommend = (resolve) => {
     resolve(module)
   })
 }
+
 const Singer = (resolve) => {
   import('components/singer/singer').then((module) => {
     resolve(module)
   })
 }
+
 const Rank = (resolve) => {
   import('components/rank/rank').then((module) => {
     resolve(module)
   })
 }
+
 const Search = (resolve) => {
   import('components/search/search').then((module) => {
     resolve(module)
   })
 }
+
 const SingerDetail = (resolve) => {
   import('components/singer-detail/singer-detail').then((module) => {
     resolve(module)
   })
 }
+
 const Disc = (resolve) => {
   import('components/disc/disc').then((module) => {
     resolve(module)
   })
 }
+
 const TopList = (resolve) => {
   import('components/top-list/top-list').then((module) => {
     resolve(module)
   })
 }
+
 const UserCenter = (resolve) => {
   import('components/user-center/user-center').then((module) => {
     resolve(module)
